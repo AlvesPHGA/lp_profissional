6cb03244b41d5303c89db8ed899eee8bfd5a4cbb
Pedro H <pedrohenriquegomesalves@gmail.com>
2022-12-27 16:28:34 -0300

    updated proj: add a new call to a new class

--- src/script/index.js
+++ src/script/index.js
@@ -1,8 +1,12 @@
 import AnimaScroll from './modules/anima-scroll.js';
 import SmoothScroll from './modules/smooth-scroll.js';
+import Modal from './modules/modal.js';
 
 const animaScroll = new AnimaScroll('[data-scroll]');
 animaScroll.init();
 
 const smoothScroll = new SmoothScroll('[data-smooth]');
 smoothScroll.init();
+
+const modal = new Modal('[data-modal]');
+modal.init();
