5852755b0fb4ed2fae20928f5517fa543e062265
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-01-05 15:31:41 -0300

    updated proj: add a new feature with name hamburger menu

--- src/script/index.js
+++ src/script/index.js
@@ -1,8 +1,12 @@
 import AnimaScroll from './modules/anima-scroll.js';
 import SmoothScroll from './modules/smooth-scroll.js';
+import MenuHamburger from './modules/hamburger-menu.js';
 
 const animaScroll = new AnimaScroll('[data-scroll]');
 animaScroll.init();
 
 const smoothScroll = new SmoothScroll('[data-smooth]');
 smoothScroll.init();
+
+const menuHamburger = new MenuHamburger('.menu__hamburger');
+menuHamburger.init();
---
+++ src/script/modules/hamburger-menu.js
@@ -0,0 +1,27 @@
+export default class MenuHamburger {
+   constructor(elementDOM) {
+      this.elementDOM = document.querySelector(elementDOM);
+   }
+
+   actionMenu() {
+      const activeMenu = document.querySelector('.menu__slice');
+      const menu = document.querySelector('.menu');
+
+      activeMenu.classList.toggle('is--active__menu');
+      menu.classList.toggle('is--active__menu');
+   }
+
+   handleTouch() {
+      this.elementDOM.addEventListener('touchstart', this.actionMenu);
+   }
+
+   eventBind() {
+      this.actionMenu = this.actionMenu.bind(this);
+   }
+
+   init() {
+      this.eventBind();
+      this.handleTouch();
+      return this;
+   }
+}
