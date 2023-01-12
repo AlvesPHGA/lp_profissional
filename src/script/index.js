80737c1cf8c7d4f2d246d34fc078067b75f51445
Pedro H <pedrohenriquegomesalves@gmail.com>
2023-01-12 11:32:03 -0300

    updated proj: updated cards in projects section to small, medium and larger device

--- src/script/index.js
+++ src/script/index.js
@@ -1,7 +1,6 @@
 import AnimaScroll from './modules/anima-scroll.js';
 import SmoothScroll from './modules/smooth-scroll.js';
 import MenuHamburger from './modules/hamburger-menu.js';
-import ActiveCard from './modules/active-card.js';
 
 const animaScroll = new AnimaScroll('[data-scroll]');
 animaScroll.init();
@@ -11,6 +10,3 @@ smoothScroll.init();
 
 const menuHamburger = new MenuHamburger('.menu__hamburger');
 menuHamburger.init();
-
-const activeCard = new ActiveCard('[data-card]');
-activeCard.init();
